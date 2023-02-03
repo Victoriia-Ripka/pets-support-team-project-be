c55a036f91bfbbed9698a95e1e9da289172283fe
Victoriia-Ripka <vikaripka@gmail.com>
2023-02-03 15:25:48 +0100

    app + dotenv

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require('./app')
+require('dotenv').config()
 
-app.listen(3000, () => {
+const { PORT } = process.env
+
+app.listen(PORT || 3030, () => {
   console.log("Server running. Use our API on port: 3000")
 })
