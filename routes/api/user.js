5202afee7720bf1561b02cfb8e2130cab55abc19
Victoriia-Ripka <vikaripka@gmail.com>
2023-02-06 11:04:34 +0100

    validation to router addpet

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const ctrl = require("../../controllers/user");
-// const { validateBody } = require("../../middlewares");
-// const { schemas } = require("../../models/user_pets");
+const { validateBody } = require("../../middlewares");
+const { schemas } = require("../../models/user_pets");
 const { authMiddleware } = require("../../middlewares/authMiddleware");
 const router = express.Router();
 
-// validateBody(schemas.addPet),
 router.get("/", authMiddleware, ctrl.getUserInfo);
-router.post("/", authMiddleware, ctrl.addPet);
+router.post("/", authMiddleware, validateBody(schemas.AddPet), ctrl.addPet);
 router.delete("/", authMiddleware, ctrl.deletePet);
 
 module.exports = router;
