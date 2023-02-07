676db2bb0beb28dd04a686b611f9b610c32feb25
Victoriia-Ripka <vikaripka@gmail.com>
2023-02-07 09:33:49 +0100

    swagger: documentation for news and services

--- controllers/news.js
+++ controllers/news.js
@@ -5,7 +5,7 @@ const getAllNews = async (req, res) => {
   const data = await News.find();
 
   if (data) {
-    res.status(200).json(data);
+    res.status(200).json({ status: "success", code: 200, data });
   } else {
     res.status(204).json({
       message: "No Content",
--- controllers/services/getAll.js
+++ controllers/services/getAll.js
@@ -1,14 +1,21 @@
-const {Service} = require("../../models");
+const { Service } = require("../../models");
 
 const getAll = async (req, res) => {
-    const service = await Service.find({});
+  const service = await Service.find({});
+
+  if (data) {
     res.json({
-        status: "success",
-        code: 200,
-        data: {
-            result: service
-        }
+      status: "success",
+      code: 200,
+      data: {
+        result: service,
+      },
     });
+  } else {
+    res.status(204).json({
+      message: "No Content",
+    });
+  }
 };
 
 module.exports = getAll;
