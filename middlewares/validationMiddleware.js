85792e9f1513cbd09f8c478bb7c358c4bb147fe2
Roman Maksimenko <romanmaksimenkodev@gmail.com>
2023-02-18 21:20:14 +0200

    update

--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -1,6 +1,9 @@
 const Joi = require('joi').extend(require('@joi/date'));
 const phoneSchema = /^\+380[0-9]{9}$/;
-const passwordSchema = /^[0-9a-zA-Z]{7,32}$/;
+// const passwordSchema = /^[0-9a-zA-Z]{7,32}$/;
+const passwordSchemaStepOne = /^(\S+$)/;
+const passwordSchemaStepTwo = /^(?=.*[a-z])(?=.*[A-Z])/;
+const passwordSchemaStepThree = /^(?=.{6,20}$)\D*\d/;
 const nameRules = /^[aA-zZ\s]+$/;
 const regionRules = /^()(\w+(,|\s)\s*)+\w+$/;
 const regionRulesOnlyLetters = /^[a-zA-Z\s]{3,},[a-zA-Z\s]{4,}$/;
@@ -11,7 +14,7 @@ const userValidation = async (req, res, next) => {
         name: Joi.string().min(3).max(30).pattern(nameRules),
         email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ua'] } }).max(40).required(),
         place: Joi.string().min(2).max(30).pattern(regionRules).pattern(regionRulesOnlyLetters),
-        password: Joi.string().min(7).max(32).required().pattern(passwordSchema),
+        password: Joi.string().min(7).max(32).required().pattern(passwordSchemaStepOne).pattern(passwordSchemaStepTwo).pattern(passwordSchemaStepThree),
         phone: Joi.string().pattern(phoneSchema),
     });
 
